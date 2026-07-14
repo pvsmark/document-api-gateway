40ad8c1675249c7080c35ec8c67d46f91eb866de
pvsmark <mronolo@propertyvaluationservices.net>
2026-07-14 12:20:04 -0500

    feat: add caller IP allowlist

---
+++ src/middleware/ipAllowlist.middleware.js
@@ -0,0 +1,20 @@
+const { createHttpError } = require('../utils/httpError');
+
+function normalizeIp(value) {
+  const selected = String(value || '').trim();
+  return selected.startsWith('::ffff:') ? selected.slice(7) : selected;
+}
+
+function createIpAllowlistMiddleware(config) {
+  const allowed = new Set(config.allowedCallerIps.map(normalizeIp));
+  return function ipAllowlistMiddleware(req, res, next) {
+    const callerIp = normalizeIp(req.ip || req.socket.remoteAddress);
+    if (!allowed.has(callerIp)) {
+      return next(createHttpError(403, 'Caller is not allowed.', 'CALLER_IP_FORBIDDEN'));
+    }
+    req.callerIp = callerIp;
+    return next();
+  };
+}
+
+module.exports = { createIpAllowlistMiddleware, normalizeIp };
