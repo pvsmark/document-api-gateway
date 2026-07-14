bcba0d61fdaf39636eafa35ce11392ca66ecf8b4
pvsmark <mronolo@propertyvaluationservices.net>
2026-07-14 16:09:19 -0500

    fix: restore document smoke test

--- scripts/test-document.ps1
+++ scripts/test-document.ps1
@@ -1,8 +1,8 @@
 param(
-  [Parameter(Mandatory=$true)][string]$Secret,
-  [Parameter(Mandatory=$true)][int]$ClientId,
-  [Parameter(Mandatory=$true)][int]$DocumentId,
-  [string]$BaseUrl = 'https://pvs-document-api.internal',
+  [Parameter(Mandatory = $true)][string]$Secret,
+  [Parameter(Mandatory = $true)][int]$ClientId,
+  [Parameter(Mandatory = $true)][int]$DocumentId,
+  [string]$BaseUrl = 'http://127.0.0.1:3100',
   [string]$KeyId = 'pvs-web2',
   [string]$OutputPath = '.\document-download.bin'
 )
@@ -10,5 +10,4 @@ param(
 Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 
-$pathAndQuery = "/v1/documents/${DocumentId}?clientId=$ClientId"
-$timestamp = [DateTimeOffset]::Utc
+$pathAndQuery = "/v1/documents/${
